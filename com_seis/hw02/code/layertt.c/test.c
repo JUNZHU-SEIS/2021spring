7dd6a008dfd8ca421d88f8af95e640aa20917ce4
JUNZHU-SEIS <Jun__Zhu@outlook.com>
2021-04-07 12:53:32 +0800

    hw02 for geodynamics

---
+++ com_seis/hw02/code/layertt.c/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	double a=3, b=40;  //为变量赋初值
+      double c = log(a);  //求3的对数
+      double d = log(b);  //求40的对数
+      printf("%lf\t%lf\n", c, d);
+      return 0;
+}
